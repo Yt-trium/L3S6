f87dd976ef19603e607bdb97f781f6b9e3969a0f
Yt-trium <cyril.meyer.68@gmail.com>
2017-03-30 13:26:16 +0200

    [SD] TP4

---
+++ SD/TP/TP4/TP4_Corba/2-ServiceBancaire/CompteImpl.java
@@ -0,0 +1,27 @@
+import java.io.* ;
+
+public class CompteImpl
+  extends ComptePOA
+{
+
+  public void deposeLiquide (float Flottant)
+  {
+
+  }
+
+  public boolean retireLiquide (float Flottant)
+  {
+    return true;
+  }
+
+  public float afficheMontant ()
+  {
+    float x = (float)42.0;
+    return x;
+  }
+
+  public boolean virementCompteaCompte (float Flottant, int Compte)
+  {
+    return true;
+  }
+}
